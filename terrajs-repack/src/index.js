1bd907b9e9180a8e6bee9ddece27ca6cbb425efd
SSen <i@ssen.name>
2021-06-07 12:21:47 +0900

    feat: terra.js re-packaging build to solve terra.js polyfill and esm problems

---
+++ terrajs-repack/src/index.js
@@ -0,0 +1,5 @@
+export * from '@terra-money/terra.js/dist/client/lcd/LCDClient';
+export * from '@terra-money/terra.js/dist/client/lcd/Wallet';
+export * from '@terra-money/terra.js/dist/client/lcd/api';
+export * from '@terra-money/terra.js/dist/extension';
+export * from '@terra-money/terra.js/dist/core';
