5580791598b337ef761ac612d4b251e87be2a1c8
SSen <i@ssen.name>
2021-08-23 19:23:04 +0900

    fix: token types

--- app/src/@nebula-js/types/tokens.ts
+++ app/src/@nebula-js/types/tokens.ts
@@ -3,6 +3,3 @@ import { NominalType } from '@libs/types';
 // CW20 currencies
 export type CT<T = string> = T & NominalType<'ct'>;
 export type NEB<T = string> = T & NominalType<'neb'>;
-
-// Union currencies
-export type CW20Token<T = string> = T & NominalType<'ct' | 'neb'>;
