42f0316656f24a8b59c0a0c0ae039fd4d6fa3ad1
SSen <i@ssen.name>
2021-11-15 14:00:40 +0900

    fix: update contract address for tests

--- app/src/@nebula-js/app-fns/test-env.ts
+++ app/src/@nebula-js/app-fns/test-env.ts
@@ -2,21 +2,21 @@ import { TEST_CONTRACT_ADDRESS as TERRA_TEST_CONTRACT_ADDRESS } from '@libs/app-
 import { CW20Addr, HumanAddr } from '@libs/types';
 
 export const TEST_CONTRACT_ADDRESS = {
-  airdrop: 'terra1z4zrm0fpuh6dq3fea3xhgf93lr30z98pzksftt' as HumanAddr,
-  collector: 'terra14xwh27azy0dkp3yceh4chcaeay5ax260puxfl7' as HumanAddr,
-  community: 'terra1fxnzfjgfvr8g6v0nxnwfjmt5vcmt64k5l5cn9y' as HumanAddr,
-  clusterFactory: 'terra1zdga7ntu73qzevtk44amefqze05k5gkwu82k05' as HumanAddr,
-  gov: 'terra17ry0egj85545rd3lllda4g48z7qrgkt4nu3ym2' as HumanAddr,
-  incentives: 'terra180sav47jd7npzc9pepqzmed2m6g3g50v44ndd5' as HumanAddr,
+  airdrop: 'terra179dgjvznfk5x2fsqa3un492l5zu9p9utdncrn7' as HumanAddr,
+  collector: 'terra1qvdf64vhtpjfznysr8uqm28exrekaju7j3mpcw' as HumanAddr,
+  community: 'terra108u9rg6nhltna8ce3teve8lsnahehljr2273m2' as HumanAddr,
+  clusterFactory: 'terra1q2ft2we9p5p5d624hsamx9k3qc2qkzw4wyg4c6' as HumanAddr,
+  gov: 'terra1qugtk3h7tr8s0ltnpheh7gaf43hv9ax7mlg6jc' as HumanAddr,
+  incentives: 'terra14j87dwkfmjs48hd3skuxnw2fm0z9x90kruspsn' as HumanAddr,
   incentivesCustody:
-    'terra16ju95fyez67xxqqlelf3wjz74kankq30z780tx' as HumanAddr,
-  staking: 'terra1gt2h9zu6c753h5ufv9a0n8hwlnpwfr2a4wyrc3' as HumanAddr,
+    'terra16stw0qmz8mj5uzg672x2t8lfdjqgva7xv3fs5q' as HumanAddr,
+  staking: 'terra1n9vdcgkd3dnqdq50kncd7e3vqj3uthg434knrd' as HumanAddr,
   terraswap: {
     factory: TERRA_TEST_CONTRACT_ADDRESS.terraswap.factory,
-    nebUstPair: 'terra1wlzgd4qa3w7ua9nhw454u7gfaxjkmlkuf3fk7f' as HumanAddr,
+    nebUstPair: 'terra17ld5j4588k8w9hhtrllyj0537eqndkhr679m72' as HumanAddr,
   },
   cw20: {
-    NEB: 'terra1c56uwrm8pukfh3q8ul460er7t32mth47gy8kmx' as CW20Addr,
+    NEB: 'terra1nknarneeatpm7amzw4rhxhcaqyt3ecx6y7k5yq' as CW20Addr,
   },
 };
 
