9d7b1d623446fece0a3e691bfd0634e84bc8765b
Another Degen <another-degen@users.noreply.github.com>
2022-02-23 16:43:59 +0700

    remove spend_limit on community pool spend

--- app/src/pages/poll/components/PollCommunityPoolSpend.tsx
+++ app/src/pages/poll/components/PollCommunityPoolSpend.tsx
@@ -1,18 +1,9 @@
-import { useCW20BalanceQuery } from '@libs/app-provider';
-import { min } from '@libs/big-math';
-import { formatUInput, formatUToken, microfy } from '@libs/formatter';
+import { microfy } from '@libs/formatter';
 import { InputAdornment } from '@material-ui/core';
-import { useCommunityConfigQuery, useNebulaApp } from '@nebula-js/app-provider';
-import { WalletIcon } from '@nebula-js/icons';
+import { useNebulaApp } from '@nebula-js/app-provider';
 import { community, gov, HumanAddr, NEB, u } from '@nebula-js/types';
-import {
-  FormLabel,
-  FormLabelAside,
-  NumberInput,
-  TextInput,
-} from '@nebula-js/ui';
+import { FormLabel, NumberInput, TextInput } from '@nebula-js/ui';
 import { AccAddress } from '@terra-money/terra.js';
-import { Big } from 'big.js';
 import React, { useCallback, useMemo, useState } from 'react';
 import { PollCreateBase } from './PollCreateBase';
 
@@ -21,34 +12,6 @@ export default function PollCommunityPoolSpend() {
 
   const [recipient, setRecipient] = useState<string>('');
   const [amount, setAmount] = useState<NEB>('' as NEB);
-
-  const { data: { tokenBalance: communityNebBalance } = {} } =
-    useCW20BalanceQuery<NEB>(
-      contractAddress.cw20.NEB,
-      contractAddress.community,
-    );
-
-  const { data: { communityConfig } = {} } = useCommunityConfigQuery();
-
-  const maxNebSpend = useMemo(() => {
-    if (!communityNebBalance || !communityConfig) {
-      return undefined;
-    }
-
-    return min(communityNebBalance.balance, communityConfig.spend_limit) as u<
-      NEB<Big>
-    >;
-  }, [communityConfig, communityNebBalance]);
-
-  const invalidAmount = useMemo(() => {
-    if (amount.length === 0 || !maxNebSpend) {
-      return undefined;
-    }
-
-    const uneb = microfy(amount as NEB);
-
-    return uneb.gt(maxNebSpend) ? 'Spending Limit Exceeded' : undefined;
-  }, [amount, maxNebSpend]);
 
   const invalidRecipient = useMemo(() => {
     if (recipient.length === 0) {
@@ -80,10 +43,7 @@ export default function PollCommunityPoolSpend() {
       description="Submit a proposal to send / receive community pool fund"
       onCreateMsg={createMsg}
       submitButtonStatus={
-        recipient.length > 0 &&
-        amount.length > 0 &&
-        !invalidAmount &&
-        !invalidRecipient
+        recipient.length > 0 && amount.length > 0 && !invalidRecipient
           ? true
           : 'disabled'
       }
@@ -99,22 +59,7 @@ export default function PollCommunityPoolSpend() {
         />
       </FormLabel>
 
-      <FormLabel
-        label="Amount of NEB to request"
-        aside={
-          maxNebSpend && (
-            <FormLabelAside
-              onClick={() =>
-                maxNebSpend && setAmount(formatUInput(maxNebSpend) as NEB)
-              }
-              style={{ color: 'var(--color-paleblue)', cursor: 'pointer' }}
-            >
-              <WalletIcon /> {formatUToken(maxNebSpend)}
-            </FormLabelAside>
-          )
-        }
-        className="form-label"
-      >
+      <FormLabel label="Amount of NEB to request" className="form-label">
         <NumberInput<NEB>
           maxIntegerPoints={14}
           maxDecimalPoints={6}
@@ -122,8 +67,6 @@ export default function PollCommunityPoolSpend() {
           fullWidth
           value={amount}
           onChange={setAmount}
-          error={!!invalidAmount}
-          helperText={invalidAmount}
           InputProps={{
             endAdornment: <InputAdornment position="end">NEB</InputAdornment>,
           }}
