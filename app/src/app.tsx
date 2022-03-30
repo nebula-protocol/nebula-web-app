b41221bf5ae89cd96aa3d00dbce42160c7206d59
Another Degen <another-degen@users.noreply.github.com>
2022-03-30 16:27:55 +0700

    Revert "disable all except gov"
    This reverts commit 0a09fc627d28629c8d753e9c9ae235fec8dee446.

--- app/src/app.tsx
+++ app/src/app.tsx
@@ -5,25 +5,25 @@ import { HoverPanel } from 'components/hover';
 import { DisableOverflowXStyle } from 'components/styles/DisableOverflowXStyle';
 import { Providers } from 'configurations/app';
 import 'configurations/chartjs';
-// import ClustersDetail from 'pages/clusters/detail';
-// import ClustersMain from 'pages/clusters/main';
-// import DashboardMain from 'pages/dashboard/main';
+import ClustersDetail from 'pages/clusters/detail';
+import ClustersMain from 'pages/clusters/main';
+import DashboardMain from 'pages/dashboard/main';
 import GovMain from 'pages/gov/main';
 import GovStake from 'pages/gov/stake';
 import GovTrade from 'pages/gov/trade';
-// import MyPageMain from 'pages/mypage/main';
-// import PollBlacklistCluster from 'pages/poll/components/PollBlacklistCluster';
-// import PollClusterParameterChange from 'pages/poll/components/PollClusterParameterChange';
-// import PollCommunityPoolSpend from 'pages/poll/components/PollCommunityPoolSpend';
-// import PollGovernanceParameterChange from 'pages/poll/components/PollGovernanceParameterChange';
-// import PollText from 'pages/poll/components/PollText';
-// import PollWhitelistClusterTextPoll from 'pages/poll/components/PollWhitelistClusterTextPoll';
+import MyPageMain from 'pages/mypage/main';
+import PollBlacklistCluster from 'pages/poll/components/PollBlacklistCluster';
+import PollClusterParameterChange from 'pages/poll/components/PollClusterParameterChange';
+import PollCommunityPoolSpend from 'pages/poll/components/PollCommunityPoolSpend';
+import PollGovernanceParameterChange from 'pages/poll/components/PollGovernanceParameterChange';
+import PollText from 'pages/poll/components/PollText';
+import PollWhitelistClusterTextPoll from 'pages/poll/components/PollWhitelistClusterTextPoll';
 import PollDetail from 'pages/poll/detail';
 import PollMain from 'pages/poll/main';
-// import Send from 'pages/send/main';
-// import StakingMain from 'pages/staking/main';
-// import StakingStake from 'pages/staking/stake';
-// import { StyleRouterTest } from 'pages/test/style-router-test';
+import Send from 'pages/send/main';
+import StakingMain from 'pages/staking/main';
+import StakingStake from 'pages/staking/stake';
+import { StyleRouterTest } from 'pages/test/style-router-test';
 import React from 'react';
 import { render } from 'react-dom';
 import { Redirect, Route, Switch } from 'react-router-dom';
@@ -37,17 +37,17 @@ function App() {
         <HoverPanel />
 
         <Switch>
-          {/* <Route exact path="/" component={DashboardMain} /> */}
-          {/* <Route path="/my" component={MyPageMain} />
+          <Route exact path="/" component={DashboardMain} />
+          <Route path="/my" component={MyPageMain} />
           <Route exact path="/clusters" component={ClustersMain} />
           <Route path="/clusters/:cluster" component={ClustersDetail} />
           <Route exact path="/staking" component={StakingMain} />
-          <Route path="/staking/:token" component={StakingStake} /> */}
+          <Route path="/staking/:token" component={StakingStake} />
           <Route exact path="/gov" component={GovMain} />
           <Route path="/gov/trade" component={GovTrade} />
           <Route path="/gov/stake" component={GovStake} />
           <Route exact path="/polls" component={PollMain} />
-          {/* <Route
+          <Route
             path="/polls/create/whitelist-cluster"
             component={PollWhitelistClusterTextPoll}
           />
@@ -67,11 +67,11 @@ function App() {
             path="/polls/create/community-pool-spend"
             component={PollCommunityPoolSpend}
           />
-          <Route path="/polls/create/text" component={PollText} /> */}
+          <Route path="/polls/create/text" component={PollText} />
           <Route path="/poll/:poll" component={PollDetail} />
-          {/* <Route path="/send" component={Send} />
-          <Route path="/test/style-router" component={StyleRouterTest} /> */}
-          <Redirect to="/gov" />
+          <Route path="/send" component={Send} />
+          <Route path="/test/style-router" component={StyleRouterTest} />
+          <Redirect to="/" />
         </Switch>
       </div>
     </div>
