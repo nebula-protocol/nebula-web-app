75634d73b9d474c2aea30fb320a20385002158ec
Another Degen <another-degen@users.noreply.github.com>
2022-04-25 18:14:58 +0700

    update inventoies section

--- app/src/@nebula-js/app-fns/logics/clusters/getTargetColor.ts
+++ app/src/@nebula-js/app-fns/logics/clusters/getTargetColor.ts
@@ -2,16 +2,16 @@ import big, { Big } from 'big.js';
 
 const DIFF_THRESHOLD = big(0.0001);
 
-// portfolio < target = #23bed9
-// portfolio > target = #f15e7e
-// |target - portfolio| < 0.01% = var(--color-white80)
+// portfolio < target = #f15e7e
+// portfolio > target = #23bed9
+// |target - portfolio| < 0.01% = var(--color-white64)
 
 export function getTargetColor(targetRatio: Big, portfolioRatio: Big): string {
   const diff = portfolioRatio.minus(targetRatio);
 
   return diff.abs().gte(DIFF_THRESHOLD)
     ? diff.gt(0)
-      ? 'var(--color-red01)'
-      : 'var(--color-blue01)'
-    : 'var(--color-white80)';
+      ? 'var(--color-blue01)'
+      : 'var(--color-red01)'
+    : 'var(--color-white64)';
 }
--- app/src/@nebula-js/ui/datavis/BarGraph.tsx
+++ app/src/@nebula-js/ui/datavis/BarGraph.tsx
@@ -1,39 +1,78 @@
-import React, { SVGProps } from 'react';
+import React, { useRef, SVGProps, useMemo, useEffect, useState } from 'react';
 import styled from 'styled-components';
+import { formatFluidDecimalPoints } from '@libs/formatter';
+import { fixHMR } from 'fix-hmr';
 
 export interface BarGraphProps extends SVGProps<SVGSVGElement> {
-  ratio: number;
-  trackColor?: string;
-  railColor?: string;
-  width: number;
-  height: number;
+  portfolioRatio: number;
+  targetRatio: number;
 }
 
 function BarGraphBase({
-  ratio,
-  trackColor,
-  railColor,
-  width,
-  height,
+  portfolioRatio,
+  targetRatio,
   ...svgProps
 }: BarGraphProps) {
+  const textRef = useRef<SVGTextElement>(null);
+  const [textX, setTextX] = useState(0);
+  const width = 420;
+  const height = 40;
+
+  const lineX = useMemo(() => {
+    return Math.min(targetRatio * width, width - 1);
+  }, [targetRatio, width]);
+
+  useEffect(() => {
+    const textWidth = textRef.current?.getBBox().width ?? 0;
+
+    if (lineX > textWidth && width - lineX > textWidth) {
+      setTextX(lineX - textWidth / 2);
+    } else if (lineX < textWidth) {
+      setTextX(lineX);
+    } else {
+      setTextX(lineX - textWidth);
+    }
+  }, [targetRatio, lineX]);
+
   return (
     <svg {...svgProps} width={width} height={height}>
+      <text x={textX} y={18} ref={textRef}>
+        Target:{' '}
+        <tspan fontWeight={700}>
+          {formatFluidDecimalPoints(targetRatio * 100, 2)}%
+        </tspan>
+      </text>
+      <rect x={0} y={28} width={width} height={16} id="bar-graph-bg" />
       <rect
         x={0}
-        y={0}
-        width={width * ratio}
-        height={height}
-        fill={railColor}
+        y={28}
+        width={width * portfolioRatio}
+        height={16}
+        id="bar-graph-bar"
       />
+      <rect x={lineX} y={22} width={1} height={18} id="bar-graph-line" />
     </svg>
   );
 }
 
-export const BarGraph = styled(BarGraphBase)`
-  background-color: ${({ trackColor }) => trackColor ?? 'var(--color-gray34)'};
+export const StyledBarGraph = styled(BarGraphBase)`
+  > text {
+    font-size: 12px;
+    fill: var(--color-white80);
+    line-height: 18px;
+  }
 
-  rect {
-    fill: ${({ railColor }) => railColor ?? 'var(--color-white80)'};
+  rect#bar-graph-line {
+    fill: var(--color-white80);
+  }
+
+  rect#bar-graph-bg {
+    fill: var(--color-gray34);
+  }
+
+  rect#bar-graph-bar {
+    fill: var(--color-blue01);
   }
 `;
+
+export const BarGraph = fixHMR(StyledBarGraph);
